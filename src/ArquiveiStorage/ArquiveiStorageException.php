95dfde8d2e6e3d094b62ca513a9d488d665f2fa0
Viviane Quinaia <viviane.quinaia@arquivei.com.br>
2020-01-28 11:37:22 -0300

    change exception construction

--- src/ArquiveiStorage/ArquiveiStorageException.php
+++ src/ArquiveiStorage/ArquiveiStorageException.php
@@ -4,5 +4,8 @@ namespace Arquivei\Flysystems\ArquiveiStorage;
 
 class ArquiveiStorageException extends \Exception
 {
-
+    public function __construct($previous, $code = 0, $message = '')
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
